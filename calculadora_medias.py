ac1cea0bdfd957d15d806c8eb11f692ceae1e167
Yuri Santos <96179643+yuwurim@users.noreply.github.com>
2023-03-22 08:02:39 -0300

    Add files via upload

---
+++ calculadora_medias.py
@@ -0,0 +1,6 @@
+print('Calculadora de média entre duas notas!!')
+
+n1=int(input('Digite sua nota de português: '))
+n2=int(input('Digite sua nota de matemática: '))
+m=(n1+n2)/2
+print('A sua média de notas é {}'.format(m))
